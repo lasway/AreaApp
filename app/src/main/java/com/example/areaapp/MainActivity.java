764737185075b26b67e052ab2b06b498b326785c
skyviolin <laswaykelvin2910@gmail.com>
2022-12-09 13:16:24 +0300

    Assignment finished

--- app/src/main/java/com/example/areaapp/MainActivity.java
+++ app/src/main/java/com/example/areaapp/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.areaapp;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    static class Circle {
+        private double radius;
+
+        private void setRadius(double r) {
+            radius = r;
+        }
+
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,68 +43,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             double area = circle.calculateArea();
             displayAreaDialogMessage(area);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             DialogMessage();
         }
     }
-    private void DialogMessage(){
+
+    private void DialogMessage() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("oups! Enter radius")
                 .setTitle("Alert Message");
 
-        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
+        builder.setPositiveButton(android.R.string.ok, (dialog, id) -> {
 
-            }
         });
-        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
+        builder.setNegativeButton(android.R.string.cancel, (dialog, id) -> {
 
-            }
         });
 
         AlertDialog dialog = builder.create();
         dialog.show();
     }
 
-    private void displayAreaDialogMessage(double area){
+    private void displayAreaDialogMessage(double area) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("The area is "+ area +" meter squared")
+        builder.setMessage("The area is " + area + " meter squared")
                 .setTitle("Area Calculated");
 
-        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
+        builder.setPositiveButton(android.R.string.ok, (dialog, id) -> {
 
-            }
         });
-        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
+        builder.setNegativeButton(android.R.string.cancel, (dialog, id) -> {
 
-            }
         });
 
         AlertDialog dialog = builder.create();
         dialog.show();
     }
 
-
-    public class Circle {
-        private double radius;
-
-        private void setRadius(double r)
-        {
-            radius = r;
-        }
-
-        public double calculateArea()
-        {
-            return Math.PI * radius * radius;
-        }
-    }
 }
